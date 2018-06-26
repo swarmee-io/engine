90b23b98d5c4d06a844b47351ef11c1121a35037
swarmee-io <40171620+swarmee-io@users.noreply.github.com>
2018-06-26 23:10:57 +0700

    create path and start crio

--- kube-standalone/setup.sh
+++ kube-standalone/setup.sh
@@ -1,5 +1,7 @@
 sudo docker swarm init
 sudo docker plugin install --grant-all-permissions weaveworks/net-plugin:2.3.0
 sudo mkdir -p /var/lib/kubelet
+sudo mkdir -p /var/log/pods
+sudo service crio start
 
 curl -sSL https://raw.githubusercontent.com/swarmee-io/swarmee/master/kube-standalone/kube_system.yaml | sudo stack deploy -c - kube
