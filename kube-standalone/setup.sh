0345a4658fa5196b5dcafa3b9b8737008afae4bb
swarmee-io <40171620+swarmee-io@users.noreply.github.com>
2018-06-26 21:35:09 +0700

    Update setup.sh

--- kube-standalone/setup.sh
+++ kube-standalone/setup.sh
@@ -1,3 +1,3 @@
 sudo docker swarm init
 sudo docker plugin install --grant-all-permissions weaveworks/net-plugin:2.3.0
-curl  | sudo stack deploy -c- kube
+curl -sSL https://raw.githubusercontent.com/swarmee-io/swarmee/master/kube-standalone/kube_system.yaml | sudo stack deploy -c- kube
