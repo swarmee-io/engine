afff80b84ea9a0f8ede8e02336a599907f9aa6d7
swarmee-io <40171620+swarmee-io@users.noreply.github.com>
2018-06-26 20:34:20 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
-echo setup.sh
+#!/bin/sh
+
+sudo add-apt-repository -y ppa:projectatomic/ppa
+sudo apt-get update
+wget -P /tmp https://github.com/swarmee-io/kengine/releases/download/17.06.2-ke-75/kengine-17.06.2-ke-75.deb
+wget -P /tmp https://raw.githubusercontent.com/weaveworks/weave/2.3/weave
+
+sudo apt-get install -y /tmp/kengine-17.06.2-ke-75.deb
+sudo service docker start
+sudo bash /tmp/weave setup
