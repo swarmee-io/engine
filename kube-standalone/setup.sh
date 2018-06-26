67efa08a7649f29bf05e653638023d7abe48c839
swarmee-io <40171620+swarmee-io@users.noreply.github.com>
2018-06-26 22:49:43 +0700

    must have a space before -

--- kube-standalone/setup.sh
+++ kube-standalone/setup.sh
@@ -2,4 +2,4 @@ sudo docker swarm init
 sudo docker plugin install --grant-all-permissions weaveworks/net-plugin:2.3.0
 sudo mkdir -p /var/lib/kubelet
 
-curl -sSL https://raw.githubusercontent.com/swarmee-io/swarmee/master/kube-standalone/kube_system.yaml | sudo stack deploy -c- kube
+curl -sSL https://raw.githubusercontent.com/swarmee-io/swarmee/master/kube-standalone/kube_system.yaml | sudo stack deploy -c - kube
