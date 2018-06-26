2724cd98441d82684d55086bb5f06e85a91b5134
swarmee-io <40171620+swarmee-io@users.noreply.github.com>
2018-06-26 22:48:41 +0700

    update stack to 0.1.1

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 
 ( cd /tmp && curl -SLO --progress-bar https://github.com/swarmee-io/kengine/releases/download/$VERSION/kengine-${VERSION}.deb )
 ( cd /tmp && curl -SLO https://raw.githubusercontent.com/weaveworks/weave/2.3/weave )
-( cd /tmp && curl -SLO https://github.com/swarmee-io/stack-deploy/releases/download/0.1.0/stack )
+( cd /tmp && curl -SLO https://github.com/swarmee-io/stack-deploy/releases/download/0.1.1/stack )
 chmod +x /tmp/stack
 
 sudo apt-get install -y /tmp/kengine-${VERSION}.deb
