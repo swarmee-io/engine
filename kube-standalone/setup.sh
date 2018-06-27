37377471d1af55a28b583598a2c21389aacfdd21
swarmee-io <40171620+swarmee-io@users.noreply.github.com>
2018-06-27 16:53:33 +0700

    Update setup.sh

--- kube-standalone/setup.sh
+++ kube-standalone/setup.sh
@@ -7,3 +7,6 @@ sudo service crio start
 URL=https://raw.githubusercontent.com/swarmee-io/swarmee/master/kube-standalone/kube_system.yaml
 
 curl -sSL $URL | sudo stack deploy -c - kube
+
+kubectl config set-cluster local --server=http://127.0.0.1:8080 --insecure-skip-tls-verify=true
+kubectl config use-context local
